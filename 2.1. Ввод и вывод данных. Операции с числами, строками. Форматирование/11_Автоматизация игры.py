5b462b58cf626619076eb746f5b0d7c170c46143
Alexey Chernous <139914095+HGRaicer@users.noreply.github.com>
2023-10-02 00:10:39 +0300

    add 2.1

---
+++ 2.1. Ввод и вывод данных. Операции с числами, строками. Форматирование/11_Автоматизация игры.py
@@ -0,0 +1,2 @@
+name = input()
+print(name[1], name[0], name[3], name[2], sep="")
---
+++ 2.1. Ввод и вывод данных. Операции с числами, строками. Форматирование/16_Доставка.py
@@ -0,0 +1,5 @@
+name1 = int(input())
+name2 = int(input())
+name3 = int(input())
+ans = (name2 - name1) / name3
+print(f"{ans:.2f}")
